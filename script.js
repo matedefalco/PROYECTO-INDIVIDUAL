8505bfc2772115a0936058899dfa296680ed5fe6
Mateo De Falco <mateodefalco1@gmail.com>
2022-03-18 19:03:23 -0300

    PRIMER COMMIT

---
+++ script.js
@@ -0,0 +1,5 @@
+const ajedrez = "https://www.chess.com/";
+window.open(ajedrez);
+
+const botonJugar = document.querySelector("#home-quicklink home-a-new_game v5-section-shadow-hover");
+botonJugar.click();
